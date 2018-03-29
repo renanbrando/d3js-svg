b2e117be8fcea22fc2f8a29efc290a4bc3a48918
Renan Brando <reebrando@gmail.com>
2018-03-29 10:53:13 -0300

    Added dinamic circles.

--- js/main.js
+++ js/main.js
@@ -3,10 +3,11 @@
 *    Mastering Data Visualization with D3.js
 *    2.5 - Activity: Adding SVGs to the screen
 */
+var data = [25,20,10,12,15];
 
 var svg = d3.select("#chart-area").append("svg")
-    .attr("width", 400)
-    .attr("height", 400);
+    .attr("width", 600)
+    .attr("height", 600);
 
 var circle = svg.append("circle")
     .attr("cx", 200)
@@ -28,3 +29,18 @@ var stroke = svg.append("line")
     .attr("y2", 100)
     .attr("stroke-width", 5)
     .attr("stroke", "black")
+
+
+var circles = svg.selectAll("circle")
+    .data(data);
+
+circles.enter()
+    .append("circle")
+        .attr("cx", function(d, i){
+            return (i * 50) + 25;
+        })
+        .attr("cy", 350)
+        .attr("r", function(d){
+            return d;
+        })
+        .enter("fill", "green");
